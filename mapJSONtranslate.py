19bc1b091520e116abb5d52d16682233c9029f12
Jared Siecinski <cooldude29073@gmail.com>
2025-08-10 15:26:38 -0400

    big commit
    added bestiary

---
+++ mapJSONtranslate.py
@@ -0,0 +1,49 @@
+import json
+
+file = open(r"assets\maps\overworld11.json", "r")
+mapJSON = json.load(file)
+file.close()
+for tile in mapJSON["tiles"]:
+    tileIdType = tile["tileId"][0:2]
+    if tileIdType == "AH":
+        tile["tileId"] = "Hills"
+    elif tileIdType == "AW":
+        tile["tileId"] = "Forest"
+    elif tileIdType == "AP":
+        tile["tileId"] = "Grasslands"
+    elif tileIdType == "AO":
+        tile["tileId"] = "Ocean"
+    elif tileIdType == "AC":
+        tile["tileId"] = "Coast"
+    elif tileIdType == "AF":
+        tile["city"] = True
+    elif tileIdType == "AV":
+        tile["river"] = True
+    elif tileIdType == "AR":
+        tile["road"] = True
+    elif tileIdType == "AB":
+        tile["road"] = True
+        tile["river"] = True
+for tile in mapJSON["tiles"]:
+    tileId = tile["tileId"]
+    if (tileId != "Hills" and tileId != "Forest" and tileId != "Grasslands" and tileId != "Ocean" and tileId != "Coast"):
+        centerX = tile["pos"]["x"]
+        centerY = tile["pos"]["y"]
+        biomeCount = [[0, 0, "Grasslands"], [0, 1, "Hills"], [0, 2, "Forest"]]
+        for searchTile in mapJSON["tiles"]:
+            searchX = searchTile["pos"]["x"]
+            searchY = searchTile["pos"]["y"]
+            if((centerY == searchY or centerY-1 == searchY) and (centerX == searchX or centerX-1 == searchX or centerX+1 == searchX)):
+                if searchTile["tileId"] == "Grasslands":
+                    biomeCount[0][0] += 1
+                elif searchTile["tileId"] == "Hills":
+                    biomeCount[1][0] += 1
+                elif searchTile["tileId"] == "Forest":
+                    biomeCount[2][0] += 1
+        tile["tileId"] = max(biomeCount)[2]
+
+file = open(r"assets\maps\overworld11.json", "w")
+json.dump(mapJSON, file)
+file.close()
+
+# (0,+1) (0,-1) (-1,-1) (-1, 0) (+1,-1) (+1,0)
